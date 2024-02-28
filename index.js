e078c19763de9f57b523b480b09d5a9b06976758
annajantzen <aejantz2015@gmail.com>
2024-02-27 19:52:29 -0600

    added tests and renders for the shapes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const {circle, square, triangle} = require('./lib/shapes')
+const {Circle, Square, Triangle} = require('./lib/shapes')
 
 // prompts for the users to enter what they want their logo to look like
 inquirer
@@ -14,16 +14,38 @@ inquirer
         {
         name: 'shapeColor',
         type: 'input',
-        message: 'Please input the color keyword that you would like for your shape color'
+        message: 'Please input the color keyword that you would like for your shape color.'
         },
         {
         name: 'text',
         type: 'input',
-        message: 'Please input up to three letters',
+        message: 'Please input up to three letters.',
         },
         {
         name: 'textColor',
         type: 'input',
-        message: 'Please input the color keyword that you would like for your text color'
-        }
+        message: 'Please input the color keyword that you would like for your text color.'
+        },
     ])
+
+    .then((answers) => {
+
+        let svg
+
+        if (answers.shape === 'Square') {
+            svg = new Square (answers.shapeColor, answers.textColor, answers.text)
+        };
+
+        if (answers.shape === 'Triange') {
+            svg = new Triangle (answers.shapeColor, answers.textColor, answers.text)
+        };
+
+        if (answers.shape === 'Circle') {
+            svg = new Circle (answers.shapeColor, answers.textColor, answers.text)
+        };
+    
+    
+    fs.writeFile('./output/logo.svg', svg.render(), (err) =>
+        err ? console.log(err) : console.log('There is a new generated logo.svg')
+        )
+    });
